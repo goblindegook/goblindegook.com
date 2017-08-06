95bcd4c932d05cf95d528892352c04f19684fd2b
Luis Rodrigues <goblindegook@users.noreply.github.com>
2017-08-07 02:12:37 +0300

    Refactor

--- themes/goblindegook/src/ts/lib/search.ts
+++ themes/goblindegook/src/ts/lib/search.ts
@@ -5,37 +5,36 @@ interface Entry extends Object {
   uri: string
 }
 
-interface SearchRepository {
-  content: Entry[], index: lunr.Index
+interface SearchIndex {
+  search (term: string): Entry[]
 }
 
-interface SearchResult extends Object {
-  score: number
-}
-
-export async function fetchIndex (url: string): Promise<SearchRepository> {
+async function fetchIndex (url: string): Promise<Entry[]> {
   const response = await fetch(url)
-  const content: Entry[] = await response.json()
+  return await response.json()
+}
 
-  const index = lunr(function () {
+function buildIndex (entries: Entry[]): lunr.Index {
+  return lunr(function () {
     this.field('title', { boost: 10 })
     this.field('categories', { boost: 3 })
     this.field('tags', { boost: 3 })
     this.field('content')
     this.ref('uri')
 
-    content.forEach((entry) => {
+    entries.forEach(entry => {
       this.add(entry)
     })
   })
-
-  return { content, index }
 }
 
-export function searchIndex (repository: SearchRepository, query: string): SearchResult[] {
-  return repository.index.search(query)
-    .map((result) => {
-      const entry = repository.content.find((e) => e.uri === result.ref)
-      return { ...entry, score: result.score }
-    })
+export async function createIndex (url: string): Promise<SearchIndex> {
+  const entries = await fetchIndex(url)
+  const index = buildIndex(entries)
+
+  return {
+    search: (query: string) => index.search(query)
+      .map(result => result.ref)
+      .map(ref => entries.find(e => e.uri === ref)!)
+  }
 }
