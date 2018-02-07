5e4194b97c2753b725905647e8df1f7a56f061b3
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-02-07 00:29:26 +0000

    Redesigned header.

--- themes/goblindegook/src/ts/setup/header.ts
+++ themes/goblindegook/src/ts/setup/header.ts
@@ -1,35 +1,11 @@
-import { debounce } from 'lodash'
-import { outerHeight } from '../lib/dom/outerHeight'
-import { createStickinessToggler } from '../lib/header'
 import { scrollTo } from '../lib/scrollTo'
-
-let scrollListener: EventListener
 
 export function setupHeader () {
   const selector = '.site-breadcrumbs'
   const breadcrumbs = document.querySelector(selector)
-  
+
   if (breadcrumbs) {
-    const headerHeight = outerHeight(document.querySelector('.single-entry-header') as HTMLElement, true)
-    const featuredImageHeight = outerHeight(document.querySelector('.single-entry-featured-image') as HTMLElement, true)
-    const content = document.querySelector('main') as HTMLElement
-  
-    const headerToggler = createStickinessToggler({
-      hiddenClass: 'bounceOutUp',
-      initialClass: 'site-breadcrumbs-initial',
-      selector,
-      threshold: content.offsetTop + headerHeight - featuredImageHeight,
-      visibleClass: 'slideInDown'
-    })
-  
-    Array.from(breadcrumbs.querySelectorAll('.trail-end, .site-title'))
+    Array.from(breadcrumbs.querySelectorAll('.trail-end, .site-title, .site-tagline'))
       .forEach((element: HTMLElement) => element.addEventListener('click', () => scrollTo(0)))
-
-    if (scrollListener) {
-      window.removeEventListener('scroll', scrollListener)
-    }
-
-    scrollListener = debounce(headerToggler, 30)  
-    window.addEventListener('scroll', scrollListener)
   }
 }
--- themes/goblindegook/src/ts/sw.ts
+++ themes/goblindegook/src/ts/sw.ts
@@ -1,4 +1,4 @@
-const CACHE_KEY = 'goblindegook-offline-v2'
+const CACHE_KEY = 'goblindegook-offline-v3'
 
 const OFFLINE_URL = '/offline/'
 
