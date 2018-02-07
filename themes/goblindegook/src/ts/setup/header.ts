a19a0cc1ffc7e0ef2708fe8c2e050cd0ecbcfe8f
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-02-07 17:49:49 +0000

    Fix scroll to top

--- themes/goblindegook/src/ts/setup/header.ts
+++ themes/goblindegook/src/ts/setup/header.ts
@@ -1,11 +1,11 @@
 import { scrollTo } from '../lib/scrollTo'
 
 export function setupHeader () {
-  const selector = '.site-breadcrumbs'
-  const breadcrumbs = document.querySelector(selector)
+  const selector = '.site-header'
+  const header = document.querySelector(selector)
 
-  if (breadcrumbs) {
-    Array.from(breadcrumbs.querySelectorAll('.trail-end, .site-title, .site-tagline'))
+  if (header) {
+    Array.from(header.querySelectorAll('.trail-end, .site-title, .site-tagline'))
       .forEach((element: HTMLElement) => element.addEventListener('click', () => scrollTo(0)))
   }
 }
