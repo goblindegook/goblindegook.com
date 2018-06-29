50ea002962c4d44d994f947182b2edfda235d919
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-06-29 10:31:25 +0100

    Fix uglify plugin use

--- themes/goblindegook/rollup.config.js
+++ themes/goblindegook/rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 
 const plugins = [
   resolve(),
