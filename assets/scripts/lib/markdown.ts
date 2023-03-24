528c8abb6536b31c8fb4e381289c197de5f54a8c
Luis Rodrigues <lrodrigues@newstore.com>
2023-03-25 00:41:48 +0100

    Upgrade packages

--- assets/scripts/lib/markdown.ts
+++ assets/scripts/lib/markdown.ts
@@ -1,10 +1,7 @@
 import snarkdown from 'snarkdown'
 import smartypants from 'smartypants'
-import DOMPurify from 'dompurify'
+import { sanitize } from 'dompurify'
 
 export function safeMarkdown(text: string): string {
-  return DOMPurify.sanitize(smartypants(snarkdown(text), 1)).replace(
-    /<\/?p>/gi,
-    ''
-  )
+  return sanitize(smartypants(snarkdown(text), 1)).replace(/<\/?p>/gi, '')
 }
--- scripts/search-index.mjs
+++ scripts/search-index.mjs
@@ -1,39 +1,36 @@
 #!/usr/bin/env node
+import { readFileSync, writeFileSync } from 'node:fs'
+import { join } from 'node:path'
 import { stemmer } from 'stemmer'
-import { readFileSync, writeFileSync } from 'fs'
-import path from 'path'
-import { JSDOM } from 'jsdom'
 import createDOMPurify from 'dompurify'
+import { JSDOM } from 'jsdom'
 import { decode } from 'html-entities'
 import { BloomSearch } from '@pacote/bloom-search'
 import { encode } from '@msgpack/msgpack'
 import stopwords from 'stopwords-en' assert { type: 'json' }
 
-const documentIndexFile = path.join('public', 'document-index.json')
-const searchIndexFile = path.join('public', 'search-index.msgpack')
+const documentIndexFile = join('public', 'document-index.json')
+const searchIndexFile = join('public', 'search-index.msgpack')
 
 const documents = JSON.parse(readFileSync(documentIndexFile, 'utf8'))
 
 console.log(`Indexing ${documents.length} documents...`)
 
-const { window } = new JSDOM('')
-const { sanitize } = createDOMPurify(window)
+const DOMPurify = createDOMPurify(new JSDOM().window)
 
-const searchIndex = new BloomSearch({
+const search = new BloomSearch({
   errorRate: 0.001,
   fields: { title: 5, description: 3, content: 1 },
   summary: ['url', 'title', 'description'],
   preprocess: (text) =>
-    decode(sanitize(String(text), { ALLOWED_TAGS: ['#text'] })),
+    decode(DOMPurify.sanitize(String(text), { ALLOWED_TAGS: ['#text'] })),
   stopwords: (term) => term.length > 1 && !stopwords.includes(term),
   stemmer,
 })
 
-documents.forEach((item) => searchIndex.add(String(item.id), item))
+documents.forEach((item) => search.add(item.id, item))
 
-const serializedSearchIndex = encode(
-  JSON.parse(JSON.stringify(searchIndex.index))
-)
+const serializedSearchIndex = encode(JSON.parse(JSON.stringify(search.index)))
 
 writeFileSync(searchIndexFile, serializedSearchIndex)
 
