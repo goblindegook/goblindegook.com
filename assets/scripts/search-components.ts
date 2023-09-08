7fa7c66a9b7d911e9f7375ec7ea5f75f10c5ea58
Luis Rodrigues <lrodrigues@newstore.com>
2023-09-08 13:38:46 +0200

    use href

--- assets/scripts/search-components.ts
+++ assets/scripts/search-components.ts
@@ -84,7 +84,7 @@ export const Search = ({
         { class: classPrefix + 'search-icon', title: 'Search' },
         use({
           'xmlns:xlink': 'http://www.w3.org/1999/xlink',
-          'xlink:href': '#icon-search',
+          href: '#icon-search',
         }),
       ),
       input({
