929e6e1f7b1fd2a4a24b1d9fd2477321849d4478
Luis Rodrigues <lrodrigues@newstore.com>
2023-09-09 09:39:54 +0200

    Trying out Bun

---
+++ bun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+curl -fsSL https://bun.sh/install | bash
+export PATH="/opt/buildhome/.bun/bin:$PATH"
+bun --version
+bun install
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import { test, expect } from 'bun:test'
 
 test('nothing', () => {
   expect(true).toBe(true)
